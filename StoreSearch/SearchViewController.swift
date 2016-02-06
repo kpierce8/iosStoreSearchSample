09a7774711bef22890af9558e74251df59fbb7ed
Ken Pierce <kpierce8@gmail.com>
2016-02-06 11:13:33 -0800

    page 113, added nsurlsession calls

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -48,18 +48,10 @@ class SearchViewController: UIViewController {
         return url!
     }
     
-    func performStoreRequestWithURL(url: NSURL) -> String? {
-        do {
-            return try String(contentsOfURL: url, encoding: NSUTF8StringEncoding)
-        } catch {
-            print("Download error: \(error)")
-            return nil
-        }
-    }
+
     
-    func parseJSON(jsonString: String) -> [String: AnyObject]? {
-        guard let data = jsonString.dataUsingEncoding(NSUTF8StringEncoding)
-            else { return nil }
+    func parseJSON(data: NSData) -> [String: AnyObject]? {
+       
         do {
             return try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String: AnyObject]
         } catch {
@@ -232,31 +224,38 @@ extension SearchViewController: UISearchBarDelegate {
         hasSearched = true
         searchResults = [SearchResult]()
         
-            
-        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
-            
-            dispatch_async(queue) {
-                
-            let url = self.urlWithSearchText(searchBar.text!)
+        let url = self.urlWithSearchText(searchBar.text!)
            
-            if let jsonString = self.performStoreRequestWithURL(url),
-                let dictionary = self.parseJSON(jsonString){
-                    
-                    self.searchResults = self.parseDictionary(dictionary)
-                    self.searchResults.sortInPlace { $0 < $1 }
-                    
-                    dispatch_async(dispatch_get_main_queue()) {
-                        self.isLoading = false
-                        self.tableView.reloadData()
-                    }
-                    return
+         let session = NSURLSession.sharedSession()
+            
+            let dataTask = session.dataTaskWithURL(url, completionHandler: {
+                data, response, error in
+                if let error = error {
+                    print("failure! \(error)")
+                } else if let httpResponse = response as? NSHTTPURLResponse
+                    where httpResponse.statusCode == 200 {
+                        if let data = data, dictionary = self.parseJSON(data) {
+                            self.searchResults =  self.parseDictionary(dictionary)
+                            self.searchResults.sortInPlace(<)
+                            
+                            dispatch_async(dispatch_get_main_queue()) {
+                                self.isLoading = false
+                                self.tableView.reloadData()
+                            }
+                            return
+                        }
+                        
+                } else {
+                     print("failure! \(response!)")
                 }
-            dispatch_async(dispatch_get_main_queue()) {
-                self.showNetworkError()
+                   dispatch_async(dispatch_get_main_queue()) {
+                    self.hasSearched = false
+                    self.isLoading = false
+                    self.tableView.reloadData()
+                    self.showNetworkError()
                 }
-
-            }
-  
+            })
+        dataTask.resume()
         }
     }
     
